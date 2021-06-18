13779ea8b092666b8d2135feeea58e2346ada079
Nazar Kovalenko <kn31132@gmail.com>
2021-06-18 15:09:34 +0300

    New version 0.1.1

--- unit1.pas
+++ unit1.pas
@@ -13,9 +13,13 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
     Label1: TLabel;
     Label2: TLabel;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
   private
 
   public
@@ -34,6 +38,16 @@ implementation
 procedure TForm1.Button1Click(Sender: TObject);
 begin
   ExecuteProcess('data/Delete_Duo.bat','');
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  ExecuteProcess('data/Delete_Mi_Credit.bat', '');
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  ExecuteProcess('data/Delete_Mi_Pay.bat', '');
 end;
 
 end.
